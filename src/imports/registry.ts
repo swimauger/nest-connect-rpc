3fa7d0f9fcb7624b9842f3611a597823afb3e629
swimauger <swimauger@gmail.com>
2025-02-17 01:34:56 -0600

    Add registry

---
+++ src/imports/registry.ts
@@ -0,0 +1,4 @@
+// import * as wkt from '@bufbuild/protobuf/wkt'; - There is a chance we need types to be discovered from the registry?
+import { createMutableRegistry } from '@bufbuild/protobuf';
+
+export const registry = createMutableRegistry();
