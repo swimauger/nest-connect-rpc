1aa9926384d29b6f6d820ec133660992cab3ae00
swimauger <swimauger@gmail.com>
2025-02-20 00:45:29 -0600

    Make decorator more strict

--- src/imports/decorators/connect-rpc.ts
+++ src/imports/decorators/connect-rpc.ts
@@ -1,6 +1,47 @@
 import { DescService } from '@bufbuild/protobuf';
-import { Reflector } from '@nestjs/core';
 
-export const ConnectRpc = Reflector.createDecorator<DescService>({
-  key: 'connect-rpc'
-});
+type ConnectRpcRequest<T extends DescService, U extends keyof T['method']> =
+  T['method'][U] extends { methodKind: 'unary' } ?
+    // @ts-ignore
+    Exclude<T['method'][U]['input']['a'], boolean> :
+  T['method'][U] extends { methodKind: 'server_streaming' } ?
+    // @ts-ignore
+    Exclude<T['method'][U]['input']['a'], boolean> :
+  T['method'][U] extends { methodKind: 'client_streaming' } ?
+    // @ts-ignore
+    AsyncGenerator<Exclude<T['method'][U]['input']['a'], boolean>> :
+  T['method'][U] extends { methodKind: 'bidi_streaming' } ?
+    // @ts-ignore
+    AsyncGenerator<Exclude<T['method'][U]['input']['a'], boolean>> :
+    void;
+
+type ConnectRpcResponse<T extends DescService, U extends keyof T['method']> =
+  T['method'][U] extends { methodKind: 'unary' } ?
+    // @ts-ignore
+    Exclude<T['method'][U]['output']['a'], boolean> :
+  T['method'][U] extends { methodKind: 'server_streaming' } ?
+    // @ts-ignore
+    Exclude<T['method'][U]['output']['a'], boolean> :
+  T['method'][U] extends { methodKind: 'client_streaming' } ?
+    // @ts-ignore
+    AsyncGenerator<Exclude<T['method'][U]['output']['a'], boolean>> :
+  T['method'][U] extends { methodKind: 'bidi_streaming' } ?
+    // @ts-ignore
+    AsyncGenerator<Exclude<T['method'][U]['output']['a'], boolean>> :
+    void;
+
+type ConnectRpcPropertyDescriptor<T extends DescService, U extends keyof T['method']> =
+  TypedPropertyDescriptor<(request: ConnectRpcRequest<T, U>) => ConnectRpcResponse<T, U>>;
+
+export function ConnectRpc<T extends DescService>(service: T) {
+  return function<U extends keyof T['method']>(
+    _target: any,
+    _propertyKey: U,
+    descriptor: ConnectRpcPropertyDescriptor<T, U>
+  ) {
+    if (descriptor.value) {
+      Reflect.defineMetadata('KEY', service, descriptor.value);
+    }
+    throw new Error('ConnectRpc must be used on a method');
+  }
+}
